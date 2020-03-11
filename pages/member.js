344f359ec3e8d0d7f54d5a3a0e543d63dfcfac1c
Allen Wong <alnaym30@gmail.com>
2020-03-11 22:10:11 +0800

    Add schema.org JSON to head

--- pages/member.js
+++ pages/member.js
@@ -1,22 +1,57 @@
 import React from 'react'
 import Head from 'next/head'
+import _ from 'lodash';
 import { useQuery } from '@apollo/react-hooks';
-import JOBS_QUERY from '../graphql/job.query';
+import MEMBER_QUERY from '../graphql/member.query';
+import MemberPage from 'components/MemberPage';
 
 
-const Home = () => {
-  const { data, loading, error } = useQuery(JOBS_QUERY);
-console.log(data, error, loading);
+const Member = ({ id, page }) => {
+  const { data, loading } = useQuery(MEMBER_QUERY, { variables: { id } });
+  if (loading) return null;
+  
+  const member = {
+    id: _.get(data, 'member.id', ''),
+    individualId: _.get(data, 'member.Individual.id', ''),
+    name: _.get(data, 'member.Individual.name_ch', ''),
+    party: _.get(data, 'member.Individual.Party.name_short_ch', ''),
+    image: `https://legco.g0vhk.io${_.get(data, 'member.Individual.image', '')}`,
+    constituencyType: _.get(data, 'member.CouncilMembershipType.category', ''),
+    constituencyDistrict: _.get(data, 'member.CouncilMembershipType.sub_category', ''),
+  };
+  
+  const personSchema = JSON.stringify({
+    '@context': 'http://schema.org/',
+    '@type': 'Person',
+    name: member.name,
+    sameAs: `/members/${member.id}`,
+    url: `/members/${member.id}`,
+    image: member.image,
+    identifier: member.id,
+    memberOf: {
+      '@type': 'Organization',
+      legalName: _.get(data, 'member.Individual.Party.name_ch', ''),
+      identifier: _.get(data, 'member.Individual.Party.id', ''),
+      alternateName: member.party,
+    },
+    jobTitle: `議員 - ${member.constituencyType} - ${member.constituencyDistrict}`,
+  });
+  
   return (
     <div>
       <Head>
-        <title>Member</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Member - {member.name}</title>
+        <meta name="description" content={`${member.name} - ${member.party}`} />
+        <meta name="keywords" content={`${member.name},${member.party},${member.constituencyDistrict},${member.constituencyType}`} />
+        <script type="application/ld+json">{personSchema}</script>
       </Head>
-      <div className="hero">
-          Hi
-      </div>
+      <MemberPage page={page} id={id} member={member}/>
     </div>
   );
 }
-export default Home;
+
+Member.getInitialProps = async function({ query: { id, page }}) {
+  return { id, page };
+};
+
+export default Member;
--- pages/members.js
+++ pages/members.js
@@ -6,29 +6,51 @@ import MEMBERS_QUERY from '../graphql/members.query';
 import { useQuery } from '@apollo/react-hooks';
 
 
-const Members = ({ members }) => {
+const Members = () => {
   const { data, loading, error } = useQuery(MEMBERS_QUERY);
   if (loading) return null;
+  const members = (data.legco_CouncilMembers || []).map(m => ({
+    id: m.id,
+    name: m.Individual.name_ch,
+    party: m.Individual.Party && m.Individual.Party.name_short_ch,
+    party_full: m.Individual.Party && m.Individual.Party.name_ch,
+    party_id: m.Individual.Party && m.Individual.Party.id,
+    image: `https://legco.g0vhk.io${m.Individual.image}`,
+    constituencyType: m.CouncilMembershipType.category,
+    constituencyDistrict: m.CouncilMembershipType.sub_category,
+    statistic: {
+      motionCount: 10,
+      voteRate: 0.98,
+      attendanceRate: 0.94,
+    }
+  }));
+  const personsSchema = JSON.stringify({
+    '@context': 'http://schema.org/',
+    "@graph": members.map(member => ({
+      '@type': 'Person',
+      name: member.name,
+      sameAs: `/members/${member.id}`,
+      url: `/members/${member.id}`,
+      image: member.image,
+      identifier: member.id,
+      memberOf: {
+        '@type': 'Organization',
+        legalName: member.party_full,
+        identifier: member.party_id,
+        alternateName: member.party,
+      },
+      jobTitle: `議員 - ${member.constituencyType} - ${member.constituencyDistrict}`,
+    }))
+  });
+  
   return (
     <div>
       <Head>
-        <title>Members</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>議員列表</title>
+        <script type="application/ld+json">{personsSchema}</script>
       </Head>
       <div className="hero">
-        <MemberListPage members={data.legco_CouncilMembers.map(m => ({
-            id: m.id,
-            name: m.Individual.name_ch,
-            party: m.Individual.Party && m.Individual.Party.name_short_ch,
-            image: `https://legco.g0vhk.io${m.Individual.image}`,
-            constituencyType: m.CouncilMembershipType.category,
-            constituencyDistrict: m.CouncilMembershipType.sub_category,
-            statistic: {
-              motionCount: 10,
-              voteRate: 0.98,
-              attendanceRate: 0.94,
-            }
-          }))} />
+        <MemberListPage members={members} />
       </div>
     </div>
   );
