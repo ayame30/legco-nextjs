3c6b8f73c3a7d86a6e2f772e0532d51a26562d25
Allen Wong <alnaym30@gmail.com>
2020-03-22 02:33:11 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ module.exports = withOffline(withSass({
       },
       {
         urlPattern: /.js/,
-        handler: 'CacheFirst'
+        handler: 'NetworkFirst'
       },
       {
         urlPattern: /graphql/,
